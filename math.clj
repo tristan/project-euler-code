e28b643acb59e92ed9709152e93694422c602690
tristan <tristan.king@gmail.com>
2008-12-23 13:47:39 +1000

    working on problem 26. not yet complete

--- math.clj
+++ math.clj
@@ -32,3 +32,54 @@
   (apply #'+ lst))
 
 (defn log10 [x] (. Math (log10 x)))
+
+(defn char-to-number [c]
+  (if (= c \.)
+    c
+    (if (or (< (int c) 48) (> (int c) 57))
+      nil
+      (- (int c) 48))))
+
+(defn get-recuring-sequence 
+  [numbers] 
+  (if (or (< (count numbers) 2) (not (zero? (rem (count numbers) 2))))
+    nil
+    (loop [nums1 numbers nums2 (rest numbers)]
+      ;(println nums1 nums2)
+      (if (nil? nums2)
+	(if (nil? (rest nums1))
+	  nil
+	  (recur (rest nums1) (rest (rest nums1))))
+	(if (= (first nums1) (first nums2))
+	  (let [sequ (take-while (fn [x] (not (nil? x))) (map (fn [x y] (if (= x y) x nil)) nums1 nums2))]
+	    (if (= (count sequ) (- (count nums1) (count nums2)))
+	      (cons sequ (reverse (take (- (- (count numbers) (count nums1)) 1) numbers)))
+	      (recur nums1 (rest nums2))))
+	  (recur nums1 (rest nums2)))))))
+; note: this can be done quicker if we know that (rest numbers) has no recuring 
+
+(defn get-recuring-sequence-if-no-recuring-sequences-in-rest [numbers]
+  (if (or (< (count numbers) 2) (not (zero? (rem (count numbers) 2))))
+    nil
+    (let [a (split-at (/ (count numbers) 2) numbers)]
+      (if (= (first a) (last a))
+	(first a)
+	nil))))
+
+(defn check-for-recuring-sequence [numbers]
+  (not (nil? (get-recuring-sequence numbers))))
+
+(defn longdiv [dividend divisor]
+  (reverse 
+  (loop [result (list) dividend (map char-to-number (seq (str dividend))) remainder 0 pushed-decimal false]
+    (if (and (nil? dividend) (zero? remainder))
+      result
+      (if (= (first dividend) \.) ; push the decimal point through
+	(recur (cons \. result) (rest dividend) remainder true)
+	(if (and (nil? dividend) (not pushed-decimal))
+	  (recur (cons \. result) nil remainder true)
+	  (if (and pushed-decimal (not (nil? (get-recuring-sequence (reverse (take-while (fn [x] (not (= x \.))) result))))))
+	    (concat (get-recuring-sequence (reverse (take-while (fn [x] (not (= x \.))) result))) '(\.) (rest (drop-while (fn [x] (not (= x \.))) result)))
+	    (let [divisee (+ (* 10 remainder) (if (nil? (first dividend)) 0 (first dividend)))]
+	      (recur (cons (floor (/ divisee divisor)) result) (rest dividend) (rem divisee divisor) pushed-decimal)))))))
+))
--- test-math.clj
+++ test-math.clj
@@ -1,4 +1,25 @@
 (load-file "math.clj")
 
-(println (list-divisors 16) " = (1 2 4 8)")
-(println (list-divisors 4) " = (1 2)")
+;(println (list-divisors 16) " = (1 2 4 8)")
+;(println (list-divisors 4) " = (1 2)")
+
+(defn test-recuring-sequence-fn []
+  (loop [ctr 1000000]
+    (if (> ctr 1000000)
+      (println "done")
+      (do
+	(get-recuring-sequence (concat (range ctr) (range ctr)))
+	(recur (inc ctr))))))
+
+(defn test-long-div []
+  (loop [ctr 1]
+    (if (> ctr 100)
+      nil
+      (do
+	(println "1 /" ctr "=" (longdiv 1 ctr))
+	(recur (inc ctr))))))
+
+(defn test-long-div []
+  (println (longdiv 1 84)))
+
+(test-long-div)
