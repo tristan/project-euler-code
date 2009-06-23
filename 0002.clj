8886cbc003fe0392455d891ea8d6ffcdd03c9e84
U-Pants\Tristan <Tristan@Pants.(none)>
2009-06-23 15:34:21 +1000

    updating problem 2 to deal with the removal of lazy-cons

--- 0002.clj
+++ 0002.clj
@@ -1,14 +1,8 @@
-(defn sum [lst]
-      (if (= (rest lst) nil)
-      	  (first lst)
-	  (+ (first lst) (sum (rest lst)))))
 (defn fib
       ([] (concat [1 2] (fib 1 2)))
-      ([a b] (lazy-cons (+ a b) (fib b (+ a b)))))
+      ([a b] (lazy-seq (cons (+ a b) (fib b (+ a b))))))
 (defn problem-two [max]
-      (sum (filter (fn [x] (even? x)) (take-while (fn [x] (<= x max)) (fib)))))
+      (apply #'+ (filter (fn [x] (even? x)) (take-while (fn [x] (<= x max)) (fib)))))
 (if (= (problem-two 89) 44)
     (println (problem-two 4000000))
     (println "tests failed"))
-      
-      
