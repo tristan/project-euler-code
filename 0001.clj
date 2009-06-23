0c33a69071bbce212fcf1cbcff942c3e4feed813
U-Pants\Tristan <Tristan@Pants.(none)>
2009-06-23 15:12:20 +1000

    simplification and optimization of problem 1

--- 0001.clj
+++ 0001.clj
@@ -1,13 +1,3 @@
-(defn problem-one-recursive [current max sum]
-      (if (< current max)
-	  (if (or (= (rem current 3) 0) (= (rem current 5) 0))
-	      (problem-one-recursive (+ current 1) max (+ sum current))
-	      (problem-one-recursive (+ current 1) max sum)
-	      )
-	  (println sum)
-	  )
-      )
-(defn problem-one [max]
-  (problem-one-recursive 1 max 0))
-(problem-one 10)
-(problem-one 1000)
+(println 
+ (apply #'+ (filter #(or (zero? (rem % 5)) (zero? (rem % 3))) (range 1000)))
+)
